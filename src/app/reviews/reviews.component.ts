dd990a6908b267eb682173d6e4611120bd4b972a
abiancu <alejandrobiancucci@gmail.com>
2018-02-08 10:44:38 -0600

    generated Md5 files

--- src/app/reviews/reviews.component.ts
+++ src/app/reviews/reviews.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { GemModel }from '../../gemModel';
 import { ReviewModel} from '../../ReviewModel';
-
+import { Md5} from 'ts-md5/dist/md5';
 
 
 @Component({
@@ -18,11 +18,20 @@ export class ReviewsComponent implements OnInit {
 
   constructor() {  }
   //TODO: this does not save reviews! I should figure out a way to send my new review to a "persisted data store"
+  
+  generateAvatarUrl(email: string) {
+    return "https://2.gravatar.com/avatar/" + Md5.hashStr(email);
+  }
+  
+  
   submitClicked() {
     this.gem.reviews.push(this.newReview);
     
     this.ngOnInit();
   }
+
+  // md5 fucntions
+ 
 
   ngOnInit() {
     this.newReview = {
